16da5269cb4e904220e02ca299ba87fb0fd32575
s-hayashi1104 <shibahas.has@gmail.com>
2024-04-06 23:49:27 +0900

    Created ArticleDTO

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -4,12 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class ArticleRestController {
 
     // GET /articles/1
     @GetMapping("/articles/{id}")
-    public String showArticle(@PathVariable("id") long id){
-        return "This is an article: id = " + id;
+    public ArticleDTO showArticle(@PathVariable("id") long id){
+        return new ArticleDTO(
+                id,
+                "This is a title: id = " + id,
+                "This is contents",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
     }
 }
