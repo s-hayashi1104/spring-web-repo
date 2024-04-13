3b2591ee149bc3be6d4e58b16b98ce0597d49d8f
s-hayashi1104 <shibahas.has@gmail.com>
2024-04-13 22:36:18 +0900

    article and entity

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,5 +1,6 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.service.article.ArticleService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,18 @@ import java.time.LocalDateTime;
 @RestController
 public class ArticleRestController {
 
+    private final ArticleService articleService = new ArticleService();
+
     // GET /articles/1
     @GetMapping("/articles/{id}")
     public ArticleDTO showArticle(@PathVariable("id") long id){
+        var entity = articleService.findById(id);
         return new ArticleDTO(
-                id,
-                "This is a title: id = " + id,
-                "This is contents",
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                entity.id(),
+                entity.title(),
+                entity.content(),
+                entity.createdAt(),
+                entity.updatedAt()
         );
     }
 }
