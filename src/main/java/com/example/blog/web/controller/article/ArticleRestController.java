0918f44583b1b36a524d43938241193c81b8a2ac
s-hayashi1104 <shibahas.has@gmail.com>
2024-04-14 19:01:50 +0900

    lombok

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,22 +1,18 @@
 package com.example.blog.web.controller.article;
 
 import com.example.blog.service.article.ArticleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalDateTime;
-
 @RestController
+@RequiredArgsConstructor
 public class ArticleRestController {
 
     private final ArticleService articleService;
-
-    public ArticleRestController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
 
     // GET /articles/1
     @GetMapping("/articles/{id}")
